7c24c84a37d2966a0be83fc091664c911f53667a
artemis031 <jcbasilio1@up.edu.ph>
2017-08-02 23:21:53 +0800

    Added util controller

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,18 @@
 var mysql = require('mysql');
+var config = require('../config');
+
+const db = mysql.createConnection(config.db);
+
+db.connect(err => {
+  if (process.env.NODE_ENV === development) {
+    if (err) {
+      console.log('Error connecting to database');
+    } else {
+      console.log('Success in connecting to database');
+    }
+  }
+});
+
+db.query(`USE ${config.db.db}`);
+
+export default db;
