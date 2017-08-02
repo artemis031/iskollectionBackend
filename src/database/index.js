763357d1e76f02db49ff3d1328024d32b5ddab6a
DavidBenavidez <ddbenavidez2@up.edu.ph>
2017-08-02 23:06:05 +0800

    Minor changes

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,16 @@
 var mysql = require('mysql');
+var config = require('../config');
+
+var db = mysql.createConnection(config.db);
+
+db.connect(err => {
+  if (err) {
+    console.log('Error connecting to database');
+  } else {
+    console.log('Success in connecting to database');
+  }
+});
+
+db.query('USE iskollection');
+
+module.exports = db;
