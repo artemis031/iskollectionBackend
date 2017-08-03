31b9e0cd972fb88a6c43e62e4d907580a34de134
DavidBenavidez <ddbenavidez2@up.edu.ph>
2017-08-03 08:53:14 +0800

    Finished editing

--- src/entities/user/user.controller.js
+++ src/entities/user/user.controller.js
@@ -38,3 +38,194 @@ exports.validateEmail = (email, id) => {
     });
   });
 };
+
+exports.getAll = () => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    SELECT
+      id,
+      firstName,
+      lastName,
+      email,
+      password
+    FROM
+      user
+    `;
+
+    var values = null;
+
+    db.query(query, values, (err, rows) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while getting all users',
+          error: err.sqlMessage
+        });
+      }
+      resolve(rows);
+    });
+  });
+};
+
+exports.getById = id => {
+  return new Promise((resolve, reject) => {
+    var query = `
+	SELECT
+      id,
+      firstName,
+      lastName,
+      email,
+      password
+    FROM
+      user
+    WHERE
+      id = ?
+    `;
+
+    var values = [id];
+
+    db.query(query, values, (err, rows) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while getting user',
+          error: err.sqlMessage
+        });
+      }
+      if (!rows.length) {
+        return reject({
+          status: 404,
+          message: 'User does not exist',
+          error: null
+        });
+      }
+      resolve(rows[0]);
+    });
+  });
+};
+
+exports.create = (body, fileName) => {
+  return new Promise((resolve, reject) => {
+    var { firstName, lastName, email, password } = body;
+
+    var query = `
+      INSERT INTO
+        user(
+        	id,
+			firstName,
+			lastName,
+			email,
+			password
+        )
+      VALUES (
+      		DEFAULT, ?, ?, ?, ?
+      )
+      `;
+
+    var values = [
+      firstName, //body.firstName
+      lastName, //body.lastName
+      email,
+      password
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while creating user',
+          error: err.sqlMessage
+        });
+      }
+
+      resolve(results.insertId);
+    });
+  });
+};
+
+exports.update = (id, body) => {
+  return new Promise((resolve, reject) => {
+    var { id, firstName, lastName, email, password } = body;
+
+    var query = `
+      UPDATE
+        user
+      SET
+        id = ?,
+        firstName = ?,
+        lastName = ?,
+        email = ?,
+        password = ?
+      WHERE
+        id = ?
+    `;
+
+    const values = [id, firstName, lastName, email, password, id];
+
+    db.query(query, values, (err, results) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while updating user',
+          error: err.sqlMessage
+        });
+      }
+
+      resolve();
+    });
+  });
+};
+
+exports.updateSignature = (id, fileName, done) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+      UPDATE
+        user
+      SET
+        signature = ?
+      WHERE
+        id = ?
+    `;
+
+    const values = [fileName, id];
+
+    db.query(query, values, (err, results) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while updating signature',
+          error: err.sqlMessage
+        });
+      }
+
+      resolve();
+    });
+  });
+};
+
+exports.removeById = id => {
+  return new Promise((resolve, reject) => {
+    const query = `
+      UPDATE
+        user
+      SET
+        dateDeleted = NOW()
+      WHERE 
+        id = ?  
+    `;
+
+    const values = [id];
+
+    db.query(query, values, (err, results) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: 'Internal server error while deleting user',
+          error: err.sqlMessage
+        });
+      }
+
+      resolve();
+    });
+  });
+};
